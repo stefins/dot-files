662f07665ef98ea96753ced8806dde2d7a2998ef
Stefin <stefin@pm.me>
2022-06-06 00:44:38 +0530

    Added cpu load info for xroot

--- scripts/xroot.sh
+++ scripts/xroot.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 export DISPLAY=:0
-
-xautolock -time 1 -locker slock & disown
-
 while true;
 do
 	DATE=$(/bin/date +"%a %b %d %H:%M")
@@ -10,7 +7,8 @@ do
 	STAT=$(/usr/bin/cat /sys/class/power_supply/BAT0/status)
 	MEM=$(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')
 	DISK=$(df -h | awk '$NF=="/home"{printf "%d/%dGB (%s)\n", $3,$2,$5}')
-	ROOT="| $BATT% |($STAT)| $MEM | $DATE | $DISK |"
+	CPU_LOAD=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')	
+	ROOT="| $BATT% (${STAT:0:3}) | $MEM | $DATE | $DISK | $CPU_LOAD |"
 	xsetroot -name "$ROOT"
 	sleep 1
 done
